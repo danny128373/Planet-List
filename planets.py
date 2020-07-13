f0729ad7ba4ee2fa8e2a89ee67323fa695e4a22e
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-13 15:25:53 -0500

    Finished exercise

--- planets.py
+++ planets.py
@@ -1,40 +1,21 @@
-# Create a tuple named zoo that contains 10 of your favorite animals.
+planet_list = ["Mercury", "Mars"]
 
-zoo = ('alligator', 'bee', 'cow', 'deer', 'eagle',
-       'fox', 'giraffe', 'horse', 'iguana', 'jaguar')
-print(zoo)
+# Use append() to add Jupiter and Saturn at the end of the list.
+planet_list.append('Jupiter')
+planet_list.append('Saturn')
+# Use the extend() method to add another list of the last two planets in our solar system to the end of the list.
+planet_list.extend(['Uranus', 'Neptune'])
 
-# Find one of your animals using the tuple.index(value) syntax on the tuple.
-# # Example
-# flowers = ("daisy", "rose")
-# flower.index("rose") # Output is 1
-print(zoo.index("jaguar"))
+# Use insert() to add Earth, and Venus in the correct order.
+planet_list.insert(1, 'Venus')
+planet_list.insert(2, 'Earth')
 
-# Determine if an animal is in your tuple by using value in tuple syntax.
-# animal_to_find = "kangaroo"
-# if animal_to_find in zoo:
-#     # Print that the animal was found
-animal_to_find = 'bee'
-found_animal = False
-
-for animal in zoo:
-    if animal == animal_to_find:
-        found_animal = True
-print(found_animal)
-
-# You can reverse engineer (unpack) a tuple into another tuple with the following syntax.
-children = list(("Sally", "Hansel", "Gretel", "Svetlana"))
-(first_child, second_child, third_child, fourth_child) = children
-print(first_child)  # Output is "Sally"
-print(second_child)  # Output is "Hansel"
-print(third_child)  # Output is "Gretel"
-print(fourth_child)  # Output is "Svetlana"
-children.extend(['Daniel', 'Felipe', 'John'])
-print(children)
-children = tuple(children)
-print(children)
-
-# Create a variable for the animals in your zoo tuple, and print them to the console.
-# Convert your tuple into a list.
-# Use extend() to add three more animals to your zoo.
-# Convert the list back into a tuple.
+# Use append() again to add Pluto to the end of the list.
+planet_list.append('Pluto')
+print(planet_list)
+# Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called rocky_planets.
+rocky_planets = planet_list[:4]
+print(rocky_planets)
+# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list.
+del planet_list[-1]
+print(planet_list)
